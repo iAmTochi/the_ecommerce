a474dd70278013ccf3333e279762669a9729bc9c
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-07-16 02:04:10 +0100

    Update custom_input.dart

--- lib/widgets/custom_input.dart
+++ lib/widgets/custom_input.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:the_ecommerce/constants.dart';
 
 class CustomInput extends StatelessWidget {
-  const CustomInput({Key? key}) : super(key: key);
+  final String? hintText;
+
+  CustomInput({required this.hintText});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextField(),
+      margin: EdgeInsets.symmetric(
+        vertical: 8.0,
+        horizontal: 24.0,
+      ),
+      decoration: BoxDecoration(
+        color: Color(0xFFf5f5f5),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hintText ?? "Hint Text ...",
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 20.0,
+          ),
+        ),
+        style: Constants.regularDarkText,
+      ),
     );
   }
 }
