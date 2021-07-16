2085478a915907b66f4840c31cc47581332b4986
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-07-16 02:59:03 +0100

    colored progressIndicator

--- lib/widgets/custom_btn.dart
+++ lib/widgets/custom_btn.dart
@@ -5,18 +5,21 @@ class CustomBtn extends StatelessWidget {
   final String? text;
   final VoidCallback? onPressed;
   final bool? outlineBtn;
+  final bool? isLoading;
 
-  CustomBtn({required this.text, this.onPressed, this.outlineBtn});
+  CustomBtn(
+      {required this.text, this.onPressed, this.outlineBtn, this.isLoading});
 
   @override
   Widget build(BuildContext context) {
     bool _outlineBtn = outlineBtn ?? false;
+    bool _isLoading = isLoading ?? false;
 
     return GestureDetector(
       onTap: onPressed,
       child: Container(
         height: 65.0,
-        alignment: Alignment.center,
+        // alignment: Alignment.center,
         decoration: BoxDecoration(
           color: _outlineBtn ? Colors.transparent : Colors.black,
           border: Border.all(
@@ -29,13 +32,34 @@ class CustomBtn extends StatelessWidget {
           horizontal: 24,
           vertical: 8,
         ),
-        child: Text(
-          text ?? 'text',
-          style: TextStyle(
-            fontSize: 16.0,
-            color: _outlineBtn ? Colors.black : Colors.white,
-            fontWeight: FontWeight.w600,
-          ),
+        child: Stack(
+          children: [
+            Visibility(
+              visible: _isLoading ? false : true,
+              child: Center(
+                child: Text(
+                  text ?? 'text',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: _outlineBtn ? Colors.black : Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            Visibility(
+              visible: _isLoading,
+              child: Center(
+                child: SizedBox(
+                  width: 30.0,
+                  height: 30.0,
+                  child: CircularProgressIndicator(
+                    color: _outlineBtn ? Colors.black : Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
